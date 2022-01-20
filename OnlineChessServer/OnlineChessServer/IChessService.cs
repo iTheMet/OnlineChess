35eaff4932d1b9597dad5e67e4167dbdca6b7256
_TheMet_ <44820844+iTheMet@users.noreply.github.com>
2022-01-20 05:57:39 +0300

    создание проекта

---
+++ OnlineChessServer/OnlineChessServer/IChessService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace OnlineChessServer
+{
+    // ПРИМЕЧАНИЕ. Можно использовать команду "Переименовать" в меню "Рефакторинг", чтобы изменить имя интерфейса "IChessService" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IChessService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
